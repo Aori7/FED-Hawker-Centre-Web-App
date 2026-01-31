576d15108378e985dc215ae9f88b9e64d40eaa48
Adawiyah <n.adawiyahisham@gmail.com>
2026-02-01 05:00:06 +0800

    modifications made and testing, replaced section and debugging

--- js/script.js
+++ js/script.js
@@ -4,41 +4,71 @@
 console.log("script loaded");
 
 // for login auth script. login.html
-const authContainer = document.getElementById('auth-container');
-const loginBtn = document.getElementById('login-tab');
-const singupBtn = document.getElementById('signup-tab');
+// const authContainer = document.getElementById('auth-container');
+// const loginBtn = document.getElementById('login-tab');
+// const singupBtn = document.getElementById('signup-tab');
 
-loginBtn.onclick = () => {
-    loginBtn.classList.add('active');
-    singupBtn.classList.remove('active');
-    console.log("login clicked");
-}
+// loginBtn.onclick = () => {
+//     loginBtn.classList.add('active');
+//     singupBtn.classList.remove('active');
+//     console.log("login clicked");
+// }
 
-singupBtn.onclick = () => {
-    singupBtn.classList.add('active');
-    loginBtn.classList.remove('active');
-    console.log("signup clicked");
-}
+// singupBtn.onclick = () => {
+//     singupBtn.classList.add('active');
+//     loginBtn.classList.remove('active');
+//     console.log("signup clicked");
+// }
 
 
 // ==================================================
 
 // initiailise the swiper 
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
+// const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-    },
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
 
-    navigation:{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+//     navigation:{
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
+
+new Swiper('.card-wrapper', {
+  // Optional parameters
+  loop: true,
+  spacebetween: 20, 
+
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    Dynamicbullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0:{
+        slidesperview: 1,
     },
+    768:{
+        slidesperview: 2,
+    },
+    1024:{
+        slidesperview: 3,
+    },
+  }
+
 });
-
-
 
 
 
